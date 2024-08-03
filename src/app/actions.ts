fac3eb89b91cbc06d6760a852ac908504d5befeb
rivea0 <58330360+rivea0@users.noreply.github.com>
2024-08-03 08:26:12 +0300

    Use try catch block to handle parsing error

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -4,16 +4,22 @@ export async function handleSubmit(prevState: any, formData: FormData) {
   const negText = formData.get('negative')
   const posText = formData.get('positive')
 
-  const response = await fetch(`${process.env.URL}/api/get-insight`, {
-    next: { revalidate: 0 }, // opt out of caching
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ negText, posText }),
-  })
+  let response = null
+  let jsonResult = null
 
-  const jsonResult = await response.json()
+  try {
+    response = await fetch(`${process.env.URL}/api/get-insight`, {
+      next: { revalidate: 0 }, // opt out of caching
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ negText, posText }),
+    })
 
+    jsonResult = await response.json()
+  } catch (err) {
+    console.log(err)
+  }
   return { message: jsonResult }
 }
